92ff9f9cff7c9b066af21133874bf37d9bbe4c34
priyanka <priyanka.goel1997@gmail.com>
2019-07-01 12:19:32 +0530

    ..

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("home_chef", "root", "", {
+const sequelize = new Sequelize("home_chef", "root", "mysql@123", {
   dialect: "mysql",
   logging: false,
   
